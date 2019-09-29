5fb3991ca2dec6d805bf4ef343435a137bdd5f2f
Nick Wyers <nicholas.wyers.17@cnu.edu>
2019-09-28 23:51:40 -0400

    dd

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -20,4 +20,49 @@
 
 //TODO define all functions in header file
 
-//TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
+//TODO look in utilities.h for useful funvoid clearArray();ctions, particularly strip_unwanted_chars!
+string v[0] = {};
+void clearArray(){
+v.clear();
+}
+
+int getArraySize(){
+return v.size();
+}
+
+std::string getArrayWordAt(int i){
+return v[i];
+}
+
+int getArrayWord_NumbOccur_At(int i){
+return i;
+}
+
+bool processFile(std::fstream &myfstream){
+return true;
+}
+
+void processLine(std::string &myString){
+
+}
+
+void processToken(std::string &token){
+
+}
+
+bool openFile(std::fstream& myfile, const std::string& myFileName,
+	std::ios_base::openmode mode = std::ios_base::in){
+return true;
+    }
+
+void closeFile(std::fstream& myfile){
+
+}
+
+int writeArraytoFile(const std::string &outputfilename){
+return 1;
+}
+
+void sortArray(constants::sortOrder so){
+
+}
